2b51b9936583b4bce9eef06a47e9d1bd5f61937f
waqas997 <waqasahmed2k15@gmail.com>
2024-05-24 17:00:59 +0500

    change in http

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -34,13 +34,13 @@ function CustomSlider() {
 
   const [loading, setLoading] = useState(true);
 
-  const imageUrl = "http://thewhitehousegame.com/public/";
+  const imageUrl = "https://thewhitehousegame.com/public/";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "http://thewhitehousegame.com/public/api/get_votter_candidate",
+          "https://thewhitehousegame.com/public/api/get_votter_candidate",
           {
             headers: {
               Accept: "application/json",
